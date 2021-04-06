ef9544f460d66137c7a6f80134f4fe05494834b7
mirzabekov00 <prog.eldar@gmail.com>
2021-04-06 21:46:19 +0200

    create post mutation

--- src/resolvers/Post.ts
+++ src/resolvers/Post.ts
@@ -1,19 +1,29 @@
 import { MyContext } from "src/types";
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { Post } from "./../entities/Post";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  async posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
   }
 
-  @Query(() => Post)
-  async post(
+  @Query(() => Post, { nullable: true })
+  post(
     @Arg("id", () => Int) id: number,
     @Ctx() { em }: MyContext
   ): Promise<Post | null> {
     return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title", () => String) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
   }
 }
