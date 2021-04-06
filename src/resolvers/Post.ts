b0fd7248cd63dfb6cd261de2a17a33b77b83fef9
mirzabekov00 <prog.eldar@gmail.com>
2021-04-06 21:42:51 +0200

    single post query

--- src/resolvers/Post.ts
+++ src/resolvers/Post.ts
@@ -1,5 +1,5 @@
 import { MyContext } from "src/types";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
 import { Post } from "./../entities/Post";
 
 @Resolver()
@@ -7,5 +7,13 @@ export class PostResolver {
   @Query(() => [Post])
   async posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post)
+  async post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
   }
 }
