db6eb46ff463113ae39eca4e1b0fcd4aa4e15946
mirzabekov00 <prog.eldar@gmail.com>
2021-04-06 22:27:44 +0200

    add login mutation, hash passwords, validate username/password and return nice errors

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,7 +1,15 @@
 import { MyContext } from "src/types";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
 import { User } from "./../entities/User";
-import { hash } from "argon2";
+import { hash, verify } from "argon2";
 
 @InputType()
 class UsernamePasswordInput {
@@ -12,22 +20,74 @@ class UsernamePasswordInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User, { nullable: true })
+  @Mutation(() => User)
   async register(
-    @Arg("options") options: UsernamePasswordInput,
+    @Arg("options", () => UsernamePasswordInput) options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
   ): Promise<User | null> {
-    const { username, password } = options || {};
-    if (!username || !password) {
-      return null;
-    }
+    const { username, password } = options;
 
     const hashedPassword = await hash(password);
     const user = em.create(User, { username, password: hashedPassword });
     await em.persistAndFlush(user);
 
     return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options", () => UsernamePasswordInput) options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const { username, password } = options;
+    const user = await em.findOne(User, { username });
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "username does not exist",
+          },
+        ],
+      };
+    }
+
+    const valid = await verify(user.password, password);
+
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "incorrect password",
+          },
+        ],
+      };
+    }
+
+    await em.persistAndFlush(user);
+
+    return { user };
   }
 }
