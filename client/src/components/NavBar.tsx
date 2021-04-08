d7cebf20c4f9227c69efc48bb648cc7110ceab79
mirzabekov00 <prog.eldar@gmail.com>
2021-04-08 21:19:54 +0200

    check if user is logged in

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,20 +1,34 @@
 import React from "react";
 import { Box, Flex, Link } from "@chakra-ui/layout";
 import NextLink from "next/link";
+import { useMeQuery } from "./../generated/graphql";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  return (
-    <Flex bg="twitter.100" p={4}>
-      <Box ml="auto">
+  const [{ data, fetching }] = useMeQuery();
+
+  let body = null;
+
+  if (fetching) {
+    body = "loading...";
+  } else if (!data?.me) {
+    body = (
+      <>
         <NextLink href="/login">
           <Link mr="5">login</Link>
         </NextLink>
         <NextLink href="/register">
           <Link mr="5">register</Link>
         </NextLink>
-      </Box>
+      </>
+    );
+  } else {
+    body = <Box>{data.me.username}</Box>;
+  }
+  return (
+    <Flex bg="twitter.100" p={4}>
+      <Box ml="auto">{body}</Box>
     </Flex>
   );
 };
