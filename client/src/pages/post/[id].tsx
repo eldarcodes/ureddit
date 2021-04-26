580a56483718528a9b1a9cd27db4e7b54fbd23f8
mirzabekov00 <prog.eldar@gmail.com>
2021-04-26 20:51:20 +0200

    client: update post

--- client/src/pages/post/[id].tsx
+++ client/src/pages/post/[id].tsx
@@ -1,24 +1,12 @@
 import { Box, Heading } from "@chakra-ui/layout";
 import { withUrqlClient } from "next-urql";
-import { useRouter } from "next/router";
 import React from "react";
 import { Layout } from "../../components/Layout";
-import { usePostQuery } from "../../generated/graphql";
 import { createUrqlClient } from "../../utils/createUrqlClient";
+import { useGetPostFromUrl } from "../../utils/useGetPostFromUrl";
 
 const Post: React.FC = ({}) => {
-  const router = useRouter();
-  const intId =
-    typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
-
-  const [{ data, error, fetching }] = usePostQuery({
-    pause: intId === -1,
-    variables: {
-      id: intId,
-    },
-  });
-  console.log(data, fetching);
-
+  const [{ data, error, fetching }] = useGetPostFromUrl();
   if (fetching) {
     return <Layout>loading...</Layout>;
   }
