017ded5f54e13c790e8496ab9eb5bd605abaff10
mirzabekov00 <prog.eldar@gmail.com>
2021-05-04 18:25:06 +0200

    feat(client): sentry init

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,18 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import theme from "../theme";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn:
+    "https://4bc54ce14502462eb02d06f67725de81@o599309.ingest.sentry.io/5749095",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 function MyApp({ Component, pageProps }: any) {
   return (
